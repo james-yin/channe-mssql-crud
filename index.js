92cc3f7c7a46265a9aed82630c181242355fb4ee
James Yin <jyin88@gmail.com>
2021-03-17 18:42:43 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+'use strict';
+const express = require('express');
+const config = require('./config');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const eventRoutes = require('./routes/eventRoutes');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use('/api', eventRoutes.routes);
+
+
+
+app.listen(config.port, () => {
+  console.log('app listening on url http://localhost:' + config.port )
+});
